c87d99569a073d40efcdebe65c05fa33cd08515d
Nylog <m.golin@hotmail.it>
2026-09-06 10:23:50 +0200

    Fix name utils.py. before: utilis.py - now: utils.py

--- agent/tools.py
+++ agent/tools.py
@@ -1,2 +1,3 @@
-from db.db_utilis import get_flight_by_number
+from db.db_utils import get_flight_by_number
 from agent.weather import get_weather, interpret_departure_weather, get_destination_weather_info
+

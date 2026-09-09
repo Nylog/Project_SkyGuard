c0380c09ae405d0b2d873b466e38bd44853a61d6
Nylog <m.golin@hotmail.it>
2026-09-09 23:51:57 +0200

    seed_user.py: add users list

--- db/seed_user.py
+++ db/seed_user.py
@@ -5,3 +5,9 @@ from werkzeug.security import generate_password_hash
 BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / "data" / "flights.db"
 SCHEMA_PATH = BASE_DIR /"db" / "schema.sql"
+
+# users: (username, password, role).
+users = [
+    ("guest_user", "guest_1234", "guest"),
+    ("admin_user", "admin_1234", "admin")
+]

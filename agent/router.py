0d26be31bc65c24d796da05a0c778bea4f8fca1f
Nylog <m.golin@hotmail.it>
2026-09-09 15:46:15 +0200

    router.py: Add classify_message function

--- agent/router.py
+++ agent/router.py
@@ -19,3 +19,25 @@ model.to(device)
 model.eval() # inference mode: disables training-specific behavior like dropout
 
 id2label = model.config.id2label  # loaded automatically, since it was saved during training
+
+def classify_message(text):
+    """
+    Classifies a single message into one of the three SkyGuard categories
+    (SOS, MAINTENANCE, SERVICE) using the fine-tuned BERT model.
+    Returns the predicted category as a string.
+    """
+
+    inputs = tokenizer(text, return_tensor = "pt", truncation = "true", padding = "max_length", max_length = 50)
+
+    inputs_on_device = {}
+    for key, value in inputs.items() :
+        inputs_on_device[key] = value.to(device)
+
+    inputs = inputs_on_device
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    predicted_id = outputs.logits.argmax(dim = 1).item()
+
+    return id2label[predicted_id]

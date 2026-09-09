5d0de9bb7581bf69639bcdc0ce1e5e66abb5fdd3
Nylog <m.golin@hotmail.it>
2026-09-10 00:14:16 +0200

    seed_user.py: add seed_users function

--- db/seed_user.py
+++ db/seed_user.py
@@ -11,3 +11,28 @@ users = [
     ("guest_user", "guest_1234", "guest"),
     ("admin_user", "admin_1234", "admin")
 ]
+
+def seed_users():
+    """
+    Creates the users table (if it does not already exist) 
+    and inserts the test accounts defined in users, 
+    hashing each password before storing it.
+    """
+
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    with open(SCHEMA_PATH, "r") as f:
+        cursor.executescript(f.read()) # ensures both tables (flights, users) exist
+
+    for username, plain_password, role in users:
+        password_hash = generate_password_hash(plain_password)
+        cursor.execute(
+            "Insert or Ignore into users (username, password_hash, role) values (?, ?, ?)",
+            (username, password_hash, role)
+        )
+
+    conn.commit()
+    conn.close()
+    print("Test user created: guest_user / admin_user")
+

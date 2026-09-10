82f670b3d4f8f37698cad97039cde16df4b03da7
Nylog <m.golin@hotmail.it>
2026-09-10 10:05:52 +0200

    app.py: initialize Flask application and configure secret_key

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,8 @@
 from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = "skyguard-dev-secret" # Used by Flask to cryptographically sign session cookies
+
+
+if __name__ == "__main__" :
+    app.run(debug = True)

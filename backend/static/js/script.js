9320e25fe8b480a99ab7ef9b39948fce3dc556d0
Nylog <m.golin@hotmail.it>
2026-09-10 23:44:11 +0200

    script.js: add DOM references

---
+++ backend/static/js/script.js
@@ -0,0 +1,16 @@
+// Grab references to all the dom elements we'll need to interract with
+
+const loginView = document.getElementById("login-view");
+const chatView = document.getElementById("chat-view");
+
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("login-button");
+const loginError = document.getElementById("login-error");
+
+const loggedInAs = document.getElementById("logged-in-as");
+const logoutButton = document.getElementById("logout-button");
+
+const chatMessage = document.getElementById("chat-message");
+const chatInput = document.getElementById("chat-input");
+const sendButton = document.getElementById("send-button");

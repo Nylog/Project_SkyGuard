a5b9211638d979a6e58578496194321c49499096
Nylog <m.golin@hotmail.it>
2026-09-10 11:42:53 +0200

    auth.py: add auth blueprint

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -7,3 +7,6 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from db.db_utils import get_user_by_username
 
+
+
+auth_bp = Blueprint("auth", __name__)  # Authentication blueprint to decouple auth routes from the main app.py

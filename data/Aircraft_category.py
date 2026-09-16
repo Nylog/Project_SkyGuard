0021573ce8af891242c182aee8660af2ef5b1374
Nylog <m.golin@hotmail.it>
2026-09-16 09:01:53 +0200

    aircraft_category.py: add aircraft category dictionary

---
+++ data/Aircraft_category.py
@@ -0,0 +1,8 @@
+aircraft_category = {
+    "Boeing 737": "narrow_body",
+    "Airbus A319": "narrow_body",
+    "Airbus A320": "narrow_body",
+    "Airbus A321": "narrow_body",
+    "Airbus A220-100": "narrow_body",
+    "Airbus A330": "wide_body",
+}
